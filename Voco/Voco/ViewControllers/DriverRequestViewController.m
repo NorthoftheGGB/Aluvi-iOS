f90f55eb3d26c945f20932d42661b406853d4470
Elliott De Aratanha <elliott@Elliotts-MacBook-Pro.local>
2014-06-05 19:25:27 -0700

    added log-in switches and custom font resources

---
+++ Voco/Voco/ViewControllers/DriverRequestViewController.m
@@ -0,0 +1,38 @@
+//
+//  DriverRequestViewController.m
+//  Voco
+//
+//  Created by Elliott De Aratanha on 6/5/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import "DriverRequestViewController.h"
+
+@interface DriverRequestViewController ()
+
+@end
+
+@implementation DriverRequestViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Voco/Voco/ViewControllers/SignUpViewController.h
@@ -0,0 +1,13 @@
+//
+//  SignUpViewController.h
+//  Voco
+//
+//  Created by Elliott De Aratanha on 6/5/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignUpViewController : UIViewController
+
+@end
