2e5dcee68a521ef7946dbe993e3d7d9d47709b65
Elliott De Aratanha <elliott@Elliotts-MacBook-Pro.local>
2014-06-05 19:49:47 -0700

    custom font implimentation files

---
+++ Voco/Voco/Controls/VCTextFieldBold.m
@@ -0,0 +1,39 @@
+//
+//  VCTextFieldBold.m
+//  Voco
+//
+//  Created by Elliott De Aratanha on 6/5/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import "VCTextFieldBold.h"
+
+@implementation VCTextFieldBold
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)awakeFromNib{
+    UIFont *customFont = [UIFont fontWithName:@"KlinicSlabMedium" size:16];
+    
+    self.font = customFont;
+}
+    
+
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
