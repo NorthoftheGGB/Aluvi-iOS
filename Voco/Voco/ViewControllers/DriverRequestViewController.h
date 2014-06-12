88a70cd770551385eaf30a185146d69da8ec7221
Deepwinter <deepwinter@winterroot.net>
2014-06-12 16:46:55 -0700

    Driver Request view controller BaseViewController

--- Voco/Voco/ViewControllers/DriverRequestViewController.h
+++ Voco/Voco/ViewControllers/DriverRequestViewController.h
@@ -7,7 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "BaseViewController.h"
 
-@interface DriverRequestViewController : UIViewController
+@interface DriverRequestViewController : BaseViewController
 
 @end
