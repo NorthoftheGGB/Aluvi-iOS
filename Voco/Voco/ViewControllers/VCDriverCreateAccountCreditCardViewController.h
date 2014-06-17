d683ef148b4ae77fd08a60823d2decacf1232aa2
Elliott D <elliott@winterroot.net>
2014-06-16 20:05:16 -0700

    driver sign up lay out first pass

---
+++ Voco/Voco/ViewControllers/VCDriverCreateAccountCreditCardViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCDriverCreateAccountCreditCardViewController.h
+//  Voco
+//
+//  Created by Elliott De Aratanha on 6/16/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCDriverCreateAccountCreditCardViewController : UIViewController
+
+@end
---
+++ Voco/Voco/ViewControllers/VCDriverCreateAccountViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCDriverCreateAccountViewController.h
+//  Voco
+//
+//  Created by Elliott De Aratanha on 6/16/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCDriverCreateAccountViewController : UIViewController
+
+@end
