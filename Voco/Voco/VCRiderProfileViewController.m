8d50b7a382e1a09c703f8fb211ad75e6a392881f
Elliott D <elliott@winterroot.net>
2014-07-16 20:20:53 -0700

    rider profile implemented

--- Voco/Voco/VCRiderProfileViewController.m
+++ Voco/Voco/VCRiderProfileViewController.m
@@ -7,8 +7,38 @@
 //
 
 #import "VCRiderProfileViewController.h"
+#import "VCNameTextField.h"
+#import "VCEmailTextField.h"
+#import "VCPasswordTextField.h"
+#import "VCTextField.h"
+#import "VCButtonFontBold.h"
+#import "VCLabelBold.h"
+#import "VCLabel.h"
 
 @interface VCRiderProfileViewController ()
+
+@property (weak, nonatomic) IBOutlet VCNameTextField *firstNameField;
+@property (weak, nonatomic) IBOutlet VCNameTextField *lastNameField;
+@property (weak, nonatomic) IBOutlet VCPasswordTextField *passwordField;
+@property (weak, nonatomic) IBOutlet VCEmailTextField *emailField;
+@property (weak, nonatomic) IBOutlet VCTextField *currentCityField;
+@property (weak, nonatomic) IBOutlet VCButtonFontBold *changeButton;
+
+@property (weak, nonatomic) IBOutlet VCLabel *referralCodeLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *socialSegmentedControl;
+
+@property (weak, nonatomic) IBOutlet UIView *newsCheckBoxView;
+@property (weak, nonatomic) IBOutlet UIView *promoCheckBoxView;
+@property (weak, nonatomic) IBOutlet UIView *receiptsCheckBoxView;
+@property (weak, nonatomic) IBOutlet VCButtonFontBold *signoutButton;
+
+
+
+
+
+- (IBAction)didTapChangeButton:(id)sender;
+- (IBAction)didTapSocialSegmentedControl:(id)sender;
+- (IBAction)didTapSignoutButton:(id)sender;
 
 @end
 
@@ -35,4 +65,12 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)didTapChangeButton:(id)sender {
+}
+
+- (IBAction)didTapSocialSegmentedControl:(id)sender {
+}
+
+- (IBAction)didTapSignoutButton:(id)sender {
+}
 @end
