53b80e882198a4a48c2da9daf63aa68b7a8a1122
Elliott D <elliott@winterroot.net>
2014-07-16 18:26:24 -0700

    additional assets, and driver reciept detail

---
+++ Voco/Voco/VCDriverRecieptDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCDriverRecieptsDetailViewController.h
+//  Voco
+//
+//  Created by Elliott De Aratanha on 7/16/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCDriverRecieptsDetailViewController : UIViewController
+
+@end
