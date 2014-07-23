b7588427a2297fc1a37d6ee69d5fc65420d14036
Elliott D <elliott@winterroot.net>
2014-07-23 00:09:35 -0700

    changed vctextfield to new font, added layout for ride confirmation, added new assets

--- Voco/Voco/Controls/VCTextField.m
+++ Voco/Voco/Controls/VCTextField.m
@@ -24,8 +24,11 @@
 
 - (void)awakeFromNib{
         
+    //old
+    //UIFont *customFont = [UIFont fontWithName:@"KlinicSlab-Light" size:[fontSize intValue] ];
     
-    UIFont *customFont = [UIFont fontWithName:@"KlinicSlab-Light" size:[fontSize intValue] ];
+    //revision 2.0 style
+    UIFont *customFont = [UIFont fontWithName:@"HelveticaNeue-Light" size:[fontSize intValue] ];
     
     [self setFont:customFont];
 
