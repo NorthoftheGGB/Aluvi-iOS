cbece9d69d733962621e64b49e5654dc9451e687
Elliott D <elliott@winterroot.net>
2014-08-05 15:35:36 -0700

    added new assets, and created new tableviewcells for menu

---
+++ Voco/Voco/VCMenuDriverClockOnTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  VCMenuDriverClockOnTableViewCell.h
+//  Voco
+//
+//  Created by Elliott De Aratanha on 8/5/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VCMenuDriverClockOnTableViewCell : UITableViewCell
+
+@end
---
+++ Voco/Voco/VCMenuDriverClockOnTableViewCell.m
@@ -0,0 +1,25 @@
+//
+//  VCMenuDriverClockOnTableViewCell.m
+//  Voco
+//
+//  Created by Elliott De Aratanha on 8/5/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import "VCMenuDriverClockOnTableViewCell.h"
+
+@implementation VCMenuDriverClockOnTableViewCell
+
+- (void)awakeFromNib
+{
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
