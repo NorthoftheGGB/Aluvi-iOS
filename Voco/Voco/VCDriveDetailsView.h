ca71aaa13aaf3375db27c2c179c3bcd4ac2f60e1
Elliott D <elliott@winterroot.net>
2014-08-09 02:56:13 -0700

    added drive details view and all relevant outlets

---
+++ Voco/Voco/VCDriveDetailsView.h
@@ -0,0 +1,23 @@
+//
+//  VCDriveDetailsView.h
+//  Voco
+//
+//  Created by Elliott De Aratanha on 8/9/14.
+//  Copyright (c) 2014 Voco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "VCLabel.h"
+#import "VCLabelBold.h"
+#import "VCButtonStandardStyle.h"
+
+@interface VCDriveDetailsView : UIView
+@property (weak, nonatomic) IBOutlet VCLabel *dateLabel;
+@property (weak, nonatomic) IBOutlet VCLabelBold *fareEarningsLabel;
+@property (weak, nonatomic) IBOutlet VCLabel *driveTimeLabel;
+@property (weak, nonatomic) IBOutlet VCLabel *driveDistanceLabel;
+@property (weak, nonatomic) IBOutlet VCLabel *numberOfPeopleLabel;
+@property (weak, nonatomic) IBOutlet VCButtonStandardStyle *confirmationButton;
+- (IBAction)didTapConfirmationButton:(id)sender;
+
+@end
