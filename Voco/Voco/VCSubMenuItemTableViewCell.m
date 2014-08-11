996757a61653131caf38be19916076b1bb4fbf3c
Elliott D <elliott@winterroot.net>
2014-08-10 17:03:28 -0700

    ui adjustments to tableview and related tableviewcells

--- Voco/Voco/VCSubMenuItemTableViewCell.m
+++ Voco/Voco/VCSubMenuItemTableViewCell.m
@@ -25,17 +25,17 @@
 - (void) select {
     self.backgroundImageView.image = [UIImage imageNamed:@"menu-item-bg-select"];
     self.arrowImageView.image = [UIImage imageNamed:@"menu-submenu-arrow-select"];
-    self.itemTitleLabel.textColor = [UIColor colorWithRed:182 green:31 blue:36 alpha:1.0];
-    self.itemTimeLabel.textColor = [UIColor colorWithRed:182 green:31 blue:36 alpha:1.0];
-    self.itemDateLabel.textColor = [UIColor colorWithRed:182 green:31 blue:36 alpha:1.0];
+    self.itemTitleLabel.textColor = [UIColor colorWithRed:(182/255.f) green:(31/255.f) blue:(36/255.f) alpha:1.0];
+    self.itemTimeLabel.textColor = [UIColor colorWithRed:(182/255.f) green:(31/255.f) blue:(36/255.f) alpha:1.0];
+    self.itemDateLabel.textColor = [UIColor colorWithRed:(182/255.f) green:(31/255.f) blue:(36/255.f) alpha:1.0];
 }
 
 - (void) deselect {
     self.backgroundImageView.image = [UIImage imageNamed:@"menu-item-bg-select"];
     self.arrowImageView.image = [UIImage imageNamed:@"menu-submenu-arrow"];
-    self.itemTitleLabel.textColor = [UIColor colorWithRed:162 green:148 blue:144 alpha:1.0];
-    self.itemTimeLabel.textColor = [UIColor colorWithRed:162 green:148 blue:144 alpha:1.0];
-    self.itemDateLabel.textColor = [UIColor colorWithRed:162 green:148 blue:144 alpha:1.0];
+    self.itemTitleLabel.textColor = [UIColor colorWithRed:(162/255.f) green:(148/255.f) blue:(144/255.f) alpha:1.0];
+    self.itemTimeLabel.textColor = [UIColor colorWithRed:(162/255.f) green:(148/255.f) blue:(144/255.f) alpha:1.0];
+    self.itemDateLabel.textColor = [UIColor colorWithRed:(162/255.f) green:(148/255.f) blue:(144/255.f) alpha:1.0];
 }
 
 @end
