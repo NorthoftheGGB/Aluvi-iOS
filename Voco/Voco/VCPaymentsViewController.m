ff8fd2483d941a310d6509cada2b6eeed26c8498
Elliott D <elliott@winterroot.net>
2014-08-26 11:11:33 -0700

    style changes to payment view controller, and constraint adjustments to receipts tvc

--- Voco/Voco/VCPaymentsViewController.m
+++ Voco/Voco/VCPaymentsViewController.m
@@ -27,10 +27,10 @@
 
 @interface VCPaymentsViewController () <STPViewDelegate>
 @property (weak, nonatomic) IBOutlet UIView *STPViewContainer;
-@property (weak, nonatomic) IBOutlet UITableView *recieptListTableView;
+//@property (weak, nonatomic) IBOutlet UITableView *recieptListTableView;
 @property (weak, nonatomic) IBOutlet VCButtonStandardStyle *updateCardButton;
 @property (weak, nonatomic) IBOutlet UILabel *cardInfoLabel;
-@property (weak, nonatomic) IBOutlet UITableView * tableView;
+//@property (weak, nonatomic) IBOutlet UITableView * tableView;
 @property (strong, nonatomic) STPView * cardView;
 @property (nonatomic) NSInteger state;
 
