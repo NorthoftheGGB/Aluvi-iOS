47935ab074b917188d28d7d00fcfe479de30e954
Deepwinter <deepwinter@winterroot.net>
2014-10-05 01:50:49 -0400

    Fixes for iOS 8

--- Voco/Voco/main.m
+++ Voco/Voco/main.m
@@ -16,8 +16,8 @@ int main(int argc, char * argv[])
     @autoreleasepool {
         
         // logentries initialization
-        //le_init();
-        //le_set_token("cb75a4af-4ae3-4c8b-8b49-76399667f045");
+        le_init();
+        le_set_token("cb75a4af-4ae3-4c8b-8b49-76399667f045");
         
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([VCAppDelegate class]));
     }
