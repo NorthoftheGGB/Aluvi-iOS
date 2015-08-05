c7b9424b7305651b7f0da55a73f7ec2594d047b6
ioe key <key.lckpk@gmail.com>
2015-08-05 19:54:17 -0400

    created and wired car info xib

---
+++ Voco/Voco/VCCarInfoViewController.m
@@ -0,0 +1,28 @@
+//
+//  VCCarInfoViewController.m
+//  Voco
+//
+//  Created by snacks on 8/5/15.
+//  Copyright (c) 2015 Voco. All rights reserved.
+//
+
+#import "VCCarInfoViewController.h"
+
+@implementation VCCarInfoViewController
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+- (IBAction)didTapLiscencePlateField:(id)sender {
+}
+- (IBAction)liscencePlateFieldDidEndOnExit:(id)sender {
+}
+
+- (IBAction)carInfoFieldDidEndOnExit:(id)sender {
+}
+@end
