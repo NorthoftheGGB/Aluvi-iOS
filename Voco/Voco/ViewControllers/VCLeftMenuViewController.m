9c36ecbf0e3e50cea65e27729f15a8b65e90f9b5
ioe key <key.lckpk@gmail.com>
2015-08-11 21:54:45 -0400

    made gradient on the menu

--- Voco/Voco/ViewControllers/VCLeftMenuViewController.m
+++ Voco/Voco/ViewControllers/VCLeftMenuViewController.m
@@ -29,6 +29,7 @@
 #import "VCUtilities.h"
 #import "VCNotifications.h"
 #import "VCReceiptViewController.h"
+#import "VCStyle.h"
 
 
 // These ones go in the array
@@ -101,11 +102,23 @@
     return self;
 }
 
+- (void) setGradient {
+    CAGradientLayer *gradient = [CAGradientLayer layer];
+    gradient.frame = self.view.bounds;
+    gradient.colors = [VCStyle gradientColors];
+    gradient.startPoint = CGPointMake(0.0, 0.5);
+    gradient.endPoint = CGPointMake(1.0, 0.5);
+    [self.view.layer insertSublayer:gradient atIndex:0];
+}
+
+
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     
+    [super viewDidLoad];
+    [self setGradient];
     
     
     // Listen for notifications about updated rides
@@ -177,7 +190,7 @@
     long row = [indexPath row];
     UITableViewCell * cell;
     
-    UIImageView *tempImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"menu-bg@2x.png"]];
+    UIImageView *tempImageView;
     [tempImageView setFrame:self.tableView.frame];
     self.tableView.backgroundView = tempImageView;
     
@@ -200,15 +213,6 @@
         }
             break;
             
-        /*case kProfileCellInteger:
-        {
-            VCMenuItemTableViewCell * menuItemTableViewCell = [WRUtilities getViewFromNib:@"VCMenuItemTableViewCell" class:[VCMenuItemTableViewCell class]];
-            menuItemTableViewCell.iconImageView.image = [UIImage imageNamed: @"menu-profile-icon"];
-            menuItemTableViewCell.menuItemLabel.text = @"Profile";
-            menuItemTableViewCell.selectionStyle = UITableViewCellSelectionStyleNone;
-            cell = menuItemTableViewCell;
-        }
-            break;*/
             
         case kScheduleCellInteger:
         {
@@ -249,7 +253,7 @@
                 subMenuItemTableViewCell.selectionStyle = UITableViewCellSelectionStyleNone;
             }
 
-            cell = subMenuItemTableViewCell;
+          //  cell = subMenuItemTableViewCell;
         }
             break;
             
