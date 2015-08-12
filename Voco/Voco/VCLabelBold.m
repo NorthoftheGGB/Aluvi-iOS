67566db15a52ba794292763469700bf7a5fe87fc
ioe key <key.lckpk@gmail.com>
2015-08-11 22:28:03 -0400

    fully updated menu styles

--- Voco/Voco/VCLabelBold.m
+++ Voco/Voco/VCLabelBold.m
@@ -29,7 +29,7 @@
     //UIFont *customFont = [UIFont fontWithName:@"KlinicSlab-Light" size:[fontSize intValue] ];
     
     //rev 2.0
-    UIFont *customFont = [UIFont fontWithName:@"HelveticaNeue-Medium" size:[fontSize intValue] ];
+    UIFont *customFont = [UIFont fontWithName:@"Bryant-Medium" size:[fontSize intValue] ];
     
     [self setFont:customFont];
     
