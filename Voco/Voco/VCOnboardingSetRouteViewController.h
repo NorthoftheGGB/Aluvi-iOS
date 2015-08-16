65a01233891c8cbfdf822b20e2c69fe43389e817
ioe key <key.lckpk@gmail.com>
2015-08-16 19:51:47 -0400

    updated login view controller styles and added first page of onboarding

---
+++ Voco/Voco/VCOnboardingSetRouteViewController.h
@@ -0,0 +1,13 @@
+//
+//  VCOnboardingSetRouteViewController.h
+//  Voco
+//
+//  Created by snacks on 8/16/15.
+//  Copyright (c) 2015 Voco. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VCOnboardingSetRouteViewController : NSObject
+
+@end
---
+++ Voco/Voco/VCOnboardingSetRouteViewController.m
@@ -0,0 +1,13 @@
+//
+//  VCOnboardingSetRouteViewController.m
+//  Voco
+//
+//  Created by snacks on 8/16/15.
+//  Copyright (c) 2015 Voco. All rights reserved.
+//
+
+#import "VCOnboardingSetRouteViewController.h"
+
+@implementation VCOnboardingSetRouteViewController
+
+@end
